40f3a4c1eef9003b1b135a4299290c4913cdf789
Alvin <alvin.sanchez27@gmail.com>
2022-09-04 13:50:01 -0400

    commit tree 6b6d5756fd8027ddbd8a90d89793c027e2860395

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,9 @@
 class Dog
-
+    # attr_reader :name,:breed
+    # attr_writer :name,:breed
+    attr_accessor :name,:breed
+    def initialize
+        @name = @name
+        @breed = @breed
+    end
 end
